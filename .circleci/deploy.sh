7b8fca26be86e85c1d1bd6e6df3e9683f1b785d6
Charlie Getzen <charlie.getzen@procore.com>
2019-03-28 09:14:07 -0700

    push to dockerhub

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -39,26 +39,11 @@ curl -L -o /tmp/docker-$VER.tgz https://download.docker.com/linux/static/stable/
 tar -xz -C /tmp -f /tmp/docker-$VER.tgz
 mv /tmp/docker/* /usr/bin
 
-echo "Install gcloud components"
-export CLOUDSDK_CORE_DISABLE_PROMPTS=1
-curl https://sdk.cloud.google.com | bash
-${HOME}/google-cloud-sdk/bin/gcloud --quiet components update
-
-echo "Configuring gcloud authentication"
-echo "${GCLOUD_SERVICE_KEY}" | base64 --decode > "${HOME}/gcloud-service-key.json"
-${HOME}/google-cloud-sdk/bin/gcloud auth activate-service-account --key-file "${HOME}/gcloud-service-key.json"
-${HOME}/google-cloud-sdk/bin/gcloud config set project "${PROJECT_NAME}"
-docker login -u _json_key -p "$(cat ${HOME}/gcloud-service-key.json)" https://gcr.io
 
 echo "Building the tiller image"
 make docker-build VERSION="${VERSION}"
 
-echo "Pushing image to gcr.io"
+echo "Pushing image to dockerhub"
+echo $DOCKER_PASSWORD | docker login -u $DOCKER_LOGIN --password-stdin
 docker push "cgetzen/tiller:${VERSION}"
 
-echo "Building helm binaries"
-make build-cross
-make dist checksum VERSION="${VERSION}"
-
-echo "Pushing binaries to gs bucket"
-${HOME}/google-cloud-sdk/bin/gsutil cp ./_dist/* "gs://${PROJECT_NAME}"
