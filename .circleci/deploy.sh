912cd592a3f0427b85fc5491592b913e87627798
Charlie Getzen <charlie.getzen@procore.com>
2019-04-17 14:16:07 -0700

    allow building

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -20,17 +20,13 @@ if [[ -n "${CIRCLE_PR_NUMBER:-}" ]]; then
   exit
 fi
 
-: ${GCLOUD_SERVICE_KEY:?"GCLOUD_SERVICE_KEY environment variable is not set"}
-: ${PROJECT_NAME:?"PROJECT_NAME environment variable is not set"}
-
 VERSION=
 if [[ -n "${CIRCLE_TAG:-}" ]]; then
   VERSION="${CIRCLE_TAG}"
 elif [[ "${CIRCLE_BRANCH:-}" == "master" ]]; then
   VERSION="canary"
 else
-  echo "Skipping deploy step; this is neither master or a tag"
-  exit
+  VERSION="${CIRCLE_SHA1}"
 fi
 
 echo "Install docker client"
@@ -39,26 +35,10 @@ curl -L -o /tmp/docker-$VER.tgz https://download.docker.com/linux/static/stable/
 tar -xz -C /tmp -f /tmp/docker-$VER.tgz
 mv /tmp/docker/* /usr/bin
 
-echo "Install gcloud components"
-export CLOUDSDK_CORE_DISABLE_PROMPTS=1
-curl https://sdk.cloud.google.com | bash
-${HOME}/google-cloud-sdk/bin/gcloud --quiet components update
-
-echo "Configuring gcloud authentication"
-echo "${GCLOUD_SERVICE_KEY}" | base64 --decode > "${HOME}/gcloud-service-key.json"
-${HOME}/google-cloud-sdk/bin/gcloud auth activate-service-account --key-file "${HOME}/gcloud-service-key.json"
-${HOME}/google-cloud-sdk/bin/gcloud config set project "${PROJECT_NAME}"
-docker login -u _json_key -p "$(cat ${HOME}/gcloud-service-key.json)" https://gcr.io
 
 echo "Building the tiller image"
 make docker-build VERSION="${VERSION}"
 
-echo "Pushing image to gcr.io"
-docker push "gcr.io/kubernetes-helm/tiller:${VERSION}"
-
-echo "Building helm binaries"
-make build-cross
-make dist checksum VERSION="${VERSION}"
-
-echo "Pushing binaries to gs bucket"
-${HOME}/google-cloud-sdk/bin/gsutil cp ./_dist/* "gs://${PROJECT_NAME}"
+echo "Pushing image to dockerhub"
+echo $DOCKER_PASSWORD | docker login -u $DOCKER_LOGIN --password-stdin
+docker push "cgetzen/tiller:${VERSION}"
